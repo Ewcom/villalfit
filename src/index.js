523e72d2d537390c0d4211ae186607457b05b34a
Emiliano Villalpando <ewc0mx1@gmail.com>
2021-12-01 14:16:52 -0600

    final villalfit

--- src/index.js
+++ src/index.js
@@ -38,9 +38,9 @@ app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
